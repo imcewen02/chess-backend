51d51a11a7a05627e684b8fd2aa93e0b1a65f2b5
imcewen <iwm2002@gmail.com>
2025-10-07 22:51:47 -0400

    Pull real account when joing queue or moving pieces

--- src/routes/gameRoutes.ts
+++ src/routes/gameRoutes.ts
@@ -2,15 +2,28 @@ import { Server, Socket } from "socket.io";
 import { getAccountFromToken } from "../utils/JwtUtil";
 import { joinQueue, movePiece } from "../services/gameService";
 import { Position } from "../models/position";
+import accountRepository from "../repositories/accountRepository"
+import { ApiError } from "../utils/ApiError";
 
 export function registerGameRoutes(socketServer: Server, socket: Socket) { 
-    socket.on("games:joinQueue", () => {
-        const account = getAccountFromToken(socket.handshake.auth.token);
+    socket.on("games:joinQueue", async () => {
+        const accountJson = getAccountFromToken(socket.handshake.auth.token);
+
+        const account = await accountRepository.getByUsername(accountJson.username);
+        if (!account) throw new ApiError(404, "User does not exist");
+
+        account.password = "";
+
         joinQueue(account);
     })
 
-    socket.on("games:movePiece", (uuid: string, origin: Position, destination: Position) => {
-        const account = getAccountFromToken(socket.handshake.auth.token);
+    socket.on("games:movePiece", async (uuid: string, origin: Position, destination: Position) => {
+        const accountJson = getAccountFromToken(socket.handshake.auth.token);
+
+        const account = await accountRepository.getByUsername(accountJson.username);
+        if (!account) throw new ApiError(404, "User does not exist");
+
+        account.password = "";
         movePiece(uuid, account, origin, destination);
     })
 }
